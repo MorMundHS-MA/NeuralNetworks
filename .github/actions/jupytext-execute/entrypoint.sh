a4faacf88eba69926d1fabd2fc80fac9621a12ad
MorMundHS-MA <MorMundHS-MA@users.noreply.github.com>
2019-10-20 13:50:00 +0200

    Improved entrypoint script to support both use cases

--- .github/actions/jupytext-execute/entrypoint.sh
+++ .github/actions/jupytext-execute/entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-bash -c "jupytext --to ipynb --execute $*"
+if [ "$1" == "exec" ]
+    then
+        bash -c "jupytext --to ipynb --execute ${@:2}"
+elif [ "$1" == "notebook" ]
+    then
+        bash -c "jupyter notebook"
+fi
