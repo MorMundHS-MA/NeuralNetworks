bf4afca57346e7895f899190add51ad824a65c46
MorMundHS-MA <MorMundHS-MA@users.noreply.github.com>
2019-10-22 15:32:46 +0200

    Change into directory before executing notebooks

--- .github/actions/jupytext-execute/entrypoint.sh
+++ .github/actions/jupytext-execute/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 if [ "$1" == "exec" ]
     then
-        bash -c "jupytext --to ipynb --execute ${@:2}"
+        if [ -n "$INPUT_DIR" ]
+            then
+                echo "Changing into $INPUT_DIR"
+                cd "$INPUT_DIR"
+        fi
+        jupytext --to ipynb --execute "${@:2}"
 elif [ "$1" == "notebook" ]
     then
-        bash -c "jupyter notebook"
+        jupyter notebook
 fi
